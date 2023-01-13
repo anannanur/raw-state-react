bc6a5b6a1007192e9a0d0da2cebdbd2eb4b380d5
anannanur <anannaanu974@gmail.com>
2023-01-13 20:23:14 +0600

    counter with usestate done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex h-screen pt-10 w-full justify-center items-center bg-red-700'>
+      <div className='bg-red-600 text-center text-white p-20 rounded'>
+        <h1 className='text-xl mb-1'>Counter</h1>
+        <p className='my-3'>{count}</p>
+        <div className='mt-5'>
+          <button onClick={() => setCount( count + 1)} className='bg-slate-700 py-2 px-5  mr-2 border-0 outline-0 rounded'>Increment</button>
+          <button onClick={()=> setCount((preState) => preState - 1)} className='bg-slate-700 py-2 px-5 border-0 outline-0 rounded'>Decrement</button>
+        </div>
+      </div>
     </div>
   );
 }
